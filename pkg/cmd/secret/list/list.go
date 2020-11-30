4cce810eb2c8cf79daecfa4771cf6216b1ec8947
vilmibm <vilmibm@neongrid.space>
2020-11-30 11:36:29 -0800

    implement list

--- pkg/cmd/secret/list/list.go
+++ pkg/cmd/secret/list/list.go
@@ -1,12 +1,15 @@
 package list
 
 import (
-	"errors"
+	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/cli/cli/api"
 	"github.com/cli/cli/internal/ghrepo"
 	"github.com/cli/cli/pkg/cmdutil"
 	"github.com/cli/cli/pkg/iostreams"
+	"github.com/cli/cli/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -47,5 +50,50 @@ func NewCmdList(f *cmdutil.Factory, runF func(*ListOptions) error) *cobra.Comman
 }
 
 func listRun(opts *ListOptions) error {
-	return errors.New("not implemented")
+	c, err := opts.HttpClient()
+	if err != nil {
+		return fmt.Errorf("could not create http client: %w", err)
+	}
+	client := api.NewClientFromHTTP(c)
+
+	baseRepo, err := opts.BaseRepo()
+	if err != nil {
+		return fmt.Errorf("could not determine base repo: %w", err)
+	}
+
+	secrets, err := getSecrets(client, baseRepo)
+	if err != nil {
+		return fmt.Errorf("failed to get secrets: %w", err)
+	}
+
+	tp := utils.NewTablePrinter(opts.IO)
+	for _, secret := range secrets {
+		tp.AddField(secret.Name, nil, nil)
+		updatedAt := fmt.Sprintf("updated %s", secret.UpdatedAt.Format("2006-Jan-02"))
+		tp.AddField(updatedAt, nil, nil)
+		tp.EndRow()
+	}
+
+	tp.Render()
+
+	return nil
+}
+
+type Secret struct {
+	Name      string
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func getSecrets(client *api.Client, repo ghrepo.Interface) ([]Secret, error) {
+	url := fmt.Sprintf("repos/%s/%s/actions/secrets", repo.RepoOwner(), repo.RepoName())
+	result := struct {
+		Secrets []Secret
+	}{}
+
+	err := client.REST(repo.RepoHost(), "GET", url, nil, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.Secrets, nil
 }
